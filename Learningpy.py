530e18ca620337d668c88f2a3bba063b2d1af7db
Miracle Effiong <miraqueeffz@gmail.com>
2020-06-27 12:03:11 -0200

    initial commit

---
+++ Learningpy.py
@@ -0,0 +1,9 @@
+print ("Hello User")
+print ("This program gives the sum of three numbers. Enjoy!!")
+first = int(input ("What is the first number?"))
+second = int(input ("What is the second number??"))
+third = int(input ("What is the third number???"))
+ans = (first + second + third)
+print ("The sum is", ans)
+print ("Thank you for using our program. We hope to see you again. Byee!!")
+ 
